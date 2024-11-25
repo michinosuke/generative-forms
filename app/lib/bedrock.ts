f428c24cd4d6f05f99e140e0da9e3a3ad955aed7
michinosuke <michiya.318.3738@gmail.com>
2024-11-26 02:17:39 +0900

    ログ出力

--- app/lib/bedrock.ts
+++ app/lib/bedrock.ts
@@ -1,6 +1,13 @@
 import { BedrockRuntimeClient } from "@aws-sdk/client-bedrock-runtime";
 
 export const getBedrockClient = async () => {
+  console.log({
+    region: "ap-northeast-1",
+    credentials: {
+      accessKeyId: process.env.ACCESS_KEY_ID!,
+      secretAccessKey: process.env.SECRET_ACCESS_KEY!,
+    },
+  });
   const bedrockClient = new BedrockRuntimeClient({
     region: "ap-northeast-1",
     credentials: {
--- app/lib/dynamodb.ts
+++ app/lib/dynamodb.ts
@@ -2,6 +2,13 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 export const getDynamoDBDocClient = async () => {
+  console.log({
+    region: "ap-northeast-1",
+    credentials: {
+      accessKeyId: process.env.ACCESS_KEY_ID!,
+      secretAccessKey: process.env.SECRET_ACCESS_KEY!,
+    },
+  });
   const dynamodbClient = new DynamoDBClient({
     region: "ap-northeast-1",
     credentials: {
