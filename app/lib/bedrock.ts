cc51719fdc038b8ebb8b804ce851e01eb885cb4e
michinosuke <michiya.318.3738@gmail.com>
2024-11-26 01:49:17 +0900

    add @aws-amplify/auth

--- app/lib/bedrock.ts
+++ app/lib/bedrock.ts
@@ -1,5 +1,8 @@
 import { BedrockRuntimeClient } from "@aws-sdk/client-bedrock-runtime";
+import { fetchAuthSession } from "@aws-amplify/auth";
 
+const { credentials } = await fetchAuthSession();
 export const bedrockClient = new BedrockRuntimeClient({
   region: "ap-northeast-1",
+  credentials,
 });
--- app/lib/dynamodb.ts
+++ app/lib/dynamodb.ts
@@ -1,5 +1,10 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { fetchAuthSession } from "@aws-amplify/auth";
 
-const dynamodbClient = new DynamoDBClient({ region: "ap-northeast-1" });
+const { credentials } = await fetchAuthSession();
+const dynamodbClient = new DynamoDBClient({
+  region: "ap-northeast-1",
+  credentials,
+});
 export const dynamodbDocClient = DynamoDBDocumentClient.from(dynamodbClient);
